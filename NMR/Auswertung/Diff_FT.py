0a4a217dfe15f65a9ba6c0658e415574033d9f79
Felix Landmeyer <felix.landmeyer@tu-dortmund.de>
2020-01-14 15:53:26 +0100

    Korrektur sollte fertig sein

--- NMR/Auswertung/Diff_FT.py
+++ NMR/Auswertung/Diff_FT.py
@@ -5,19 +5,39 @@ import matplotlib as mpl
 from scipy.signal import find_peaks_cwt
 
 
-times, imag_v, real_v = np.genfromtxt('../data/scope_80.csv', unpack=True, delimiter=',')
+times_v, imag_v, real_v = np.genfromtxt('../data/scope_80.csv', unpack=True, delimiter=',')
 
 #Suchen des Echo-Maximums und alle Daten davor abschneiden
-start = np.argmax(real_v)
-print('Startwert:',start)
-times = times[start:]
+start = np.argmin(imag_v)
+print('Startindice:',start)
+print('Startwert:',imag_v[start])
+times = times_v[start:]
 real = real_v[start:]
 imag = imag_v[start:]
+
+#Echo vor Abschneiden und Phasenkorrektur
+plt.plot(times_v,real_v,'-',color='indianred', label='Realteil')
+plt.plot(times_v,imag_v, '-', color='dodgerblue', label='Imaginärteil')
+#plt.xlim(0,times_v[-1])
+plt.xlabel(r'$t\,\, / \,\, \mathrm{s}$')
+plt.ylabel(r'$Signalstärke \,\, / \,\, \mathrm{V}$')
+plt.legend(loc='best')
+plt.savefig("echo_vorher.pdf", bbox_inches = "tight")
+plt.clf()
+
+#Echo nach Abschneiden
+plt.plot(times,real,'-',color='indianred', label='Realteil')
+plt.plot(times,imag, '-', color='dodgerblue', label='Imaginärteil')
+plt.xlabel(r'$t\,\, / \,\, \mathrm{s}$')
+plt.ylabel(r'$Signalstärke \,\, / \,\, \mathrm{V}$')
+plt.legend(loc='best')
+plt.savefig("echo_abgeschnitten.pdf", bbox_inches = "tight")
+plt.clf()
 
 #Phasenkorrektur - der Imaginärteil bei t=0 muss = 0 sein
 phase = np.arctan2(imag[0], real[0])
 
-#Daten in komplexes Array mit Phasenkorrektur speichern 
+#Daten in komplexes Array mit Phasenkorrektur speichern
 compsignal = (real*np.cos(phase)+imag*np.sin(phase))+(-real*np.sin(phase)+imag*np.cos(phase))*1j
 
 #Offsetkorrektur, ziehe den Mittelwert der letzten 512 Punkte von allen Punkten ab
@@ -29,7 +49,7 @@ compsignal[0] = compsignal[0]/2.0
 #Anwenden einer Fensterfunktion (siehe z. Bsp. #https://de.wikipedia.org/wiki/Fensterfunktion )
 #Hier wird eine Gaußfunktion mit sigma = 100 Hz verwendet
 apodisation = 100.0*2*np.pi
-compsignal = compsignal*np.exp(-1.0/2.0*((times-times[0])*apodisation)**2) 
+compsignal = compsignal*np.exp(-1.0/2.0*((times-times[0])*apodisation)**2)
 
 #Durchführen der Fourier-Transformation
 fftdata = np.fft.fftshift(np.fft.fft(compsignal))
@@ -41,41 +61,30 @@ freqs = np.fft.fftshift(np.fft.fftfreq(len(compsignal), times[1]-times[0]))
 np.savetxt("echo_gradient_fft.txt", np.array([freqs, np.real(fftdata), np.imag(fftdata)]).transpose())
 
 
-#Echo vor Abschneiden und Phasenkorrektur
-#plt.plot(times,real_v,'-',color='indianred', label='Realteil')
-#plt.plot(times,imag_v, '-', color='dodgerblue', label='Imaginärteil')
-#plt.xlabel(r'$t\,\, / \,\, \mathrm{s}$')
-#plt.ylabel(r'$Signalstärke \,\, / \,\, \mathrm{V}$')
-#plt.legend(loc='best')
-#plt.savefig("echo_vorher.pdf")
-
-#Echo nach Abschneiden
-#plt.plot(times,real,'-',color='indianred', label='Realteil')
-#plt.plot(times,imag, '-', color='dodgerblue', label='Imaginärteil')
-#plt.xlabel(r'$t\,\, / \,\, \mathrm{s}$')
-#plt.ylabel(r'$Signalstärke \,\, / \,\, \mathrm{V}$')
-#plt.legend(loc='best')
-#plt.savefig("echo_abgeschnitten.pdf")
 
 test=freqs/1000
-test_x=[test[262],test[274]]
-test_y=[np.real(fftdata)[262],np.real(fftdata)[274]]
-print('Frequenzverschiebung =', test[274]-test[262], 'kHz')
+shoulder_low = 283
+shoulder_high = 292
+test_x=[test[shoulder_low],test[shoulder_high]]
+test_y=[np.real(fftdata)[shoulder_low],np.real(fftdata)[shoulder_high]]
 
+print('Frequenzverschiebung =', test[shoulder_high]-test[shoulder_low], 'kHz')
+
+#Plott zur Frequenz
 plt.plot((freqs/1000), np.real(fftdata), 'x', color='grey', label='Werte der Fouriertrafo')
-plt.plot(test_x,test_y, 'rx')
+plt.plot(test_x,test_y, 'rx', color = "red")
 plt.xlim(-20, 20)
 plt.xlabel(r'$Frequenzverschiebung \,\, / \,\, \mathrm{kHz}$')
 plt.ylabel(r'$Intensität$')
 plt.legend(loc='upper left', prop={'size': 9})
 #plt.show()
-plt.savefig("echo_ft.pdf")
+plt.savefig("echo_ft.pdf", bbox_inches = "tight")
+plt.clf()
 
-'''
-plt.plot(t, U1, 'x',label='Messwerte', color='indianred', linewidth=0.5)
-plt.plot(t, U2, 'x',label='Messwerte', color='dodgerblue', linewidth=0.5)
+#Plott nach Phasenkorrektur
+plt.plot(times, np.imag(compsignal), '-',label='Realteil',  color='indianred')
+plt.plot(times, -np.real(compsignal), '-',label='Imaginärteil',color='dodgerblue')
 plt.xlabel(r'$t\,\, / \,\, \mathrm{s}$')
 plt.ylabel(r'$Signalstärke \,\, / \,\, \mathrm{V}$')
 plt.legend(loc='best')
-plt.show()
-'''
+plt.savefig("echo_phasenkorrektur.pdf", bbox_inches = "tight")
