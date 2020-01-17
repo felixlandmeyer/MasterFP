98c1aa269db51b6f9e36d1ae9d9629acfa51c41e
Felix Landmeyer <felix.landmeyer@tu-dortmund.de>
2020-01-17 13:21:41 +0100

    Plot für effektive Masse verändert

--- Faraday/Auswertung/eff_Mass.py
+++ Faraday/Auswertung/eff_Mass.py
@@ -15,15 +15,15 @@ mpl.rcParams.update({'font.family': 'serif',
                     'pgf.rcfonts': False,
                     'pgf.texsystem': 'lualatex',
                     'pgf.preamble': r'\usepackage{unicode-math} \usepackage{siunitx}',})
-#font = {'family': 'normal',
-#        'weight': 'normal',
-#        'size': 12}
+font = {'family': 'normal',
+        'weight': 'normal',
+        'size': 14}
 #
-#plt.rc('font', **font)
-#plt.rc('text', usetex=True)
-#plt.rc('font', family='serif')
+plt.rc('font', **font)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 #
-#os.system('mkdir -p Plots')
+os.system('mkdir -p Plots')
 
 def Messung(path, probe_length): #probe_lentgh in meter
     lam, phi_1, min_1, phi_2, min_2 = np.genfromtxt(path, skip_header = 1, unpack =True)
@@ -62,33 +62,54 @@ Phi_probe_2 = Messung("data/Probe_2.txt", probe_length_dotiert2)[0]
 delta_phi_1 = Phi_probe_1 - Phi_probe_rein
 delta_phi_2 = Phi_probe_2 - Phi_probe_rein
 
-def lin(x, A, B):
-    return A * x**2 + B
+#def lin(x, A,B):
+#    return A * x**2 + B
+#
+#params_delta_phi_1, cov = curve_fit(lin, lam, delta_phi_1, p0 = [10e13,2])
+#errors_delta_phi_1 = np.sqrt(np.diag(cov))
+#
+#A_delta_phi_1 = ufloat(params_delta_phi_1[0],errors_delta_phi_1[0])
+#B_delta_phi_1 = ufloat(params_delta_phi_1[1],errors_delta_phi_1[1])
+#
+#params_delta_phi_2, cov = curve_fit(lin, lam, delta_phi_2,p0 = [10e13,2])
+#errors_delta_phi_2 = np.sqrt(np.diag(cov))
+#
+#A_delta_phi_2 = ufloat(params_delta_phi_2[0],errors_delta_phi_2[0])
+#B_delta_phi_2 = ufloat(params_delta_phi_2[1],errors_delta_phi_2[1])
 
-params_delta_phi_1, cov = curve_fit(lin, lam, delta_phi_1, p0 = [10e13,2])
+def lin(x, A):
+    return A * x**2
+
+params_delta_phi_1, cov = curve_fit(lin, lam, delta_phi_1, p0 = [10e13])
 errors_delta_phi_1 = np.sqrt(np.diag(cov))
 
 A_delta_phi_1 = ufloat(params_delta_phi_1[0],errors_delta_phi_1[0])
-B_delta_phi_1 = ufloat(params_delta_phi_1[1],errors_delta_phi_1[1])
 
-params_delta_phi_2, cov = curve_fit(lin, lam, delta_phi_2,p0 = [10e13,2])
+params_delta_phi_2, cov = curve_fit(lin, lam, delta_phi_2,p0 = [10e13])
 errors_delta_phi_2 = np.sqrt(np.diag(cov))
 
 A_delta_phi_2 = ufloat(params_delta_phi_2[0],errors_delta_phi_2[0])
-B_delta_phi_2 = ufloat(params_delta_phi_2[1],errors_delta_phi_2[1])
 
 eff_m_1 = unp.sqrt( cont.e**3 * N_1 * B*10**-3 / (8 * np.pi**2 * cont.c**3 * cont.epsilon_0 * n * A_delta_phi_1) )
 eff_m_2 = unp.sqrt( cont.e**3 * N_2 * B*10**-3 / (8 * np.pi**2 * cont.c**3 * cont.epsilon_0 * n * A_delta_phi_2) )
 
+mean_eff = np.mean([eff_m_1,eff_m_2])
+
 V_eff_1 = eff_m_1 / cont.m_e
 V_eff_2 = eff_m_2 / cont.m_e
+
+mean_V = np.mean([V_eff_1,V_eff_2])
 
 print(f"normierte Winkeldifferenz Hochrein:\n {Phi_probe_rein * 10**-3} in rad/mm")
 print(f"normierte Winkeldifferenz Probe 1:\n {Phi_probe_1 * 10**-3}in rad/mm")
 print(f"normierte Winkeldifferenz Probe 2:\n {Phi_probe_2 * 10**-3}in rad/mm")
-
-print(f"Probe 1 \n A = {A_delta_phi_1 * 10 **-9} in rad/mm3 \n B = {B_delta_phi_1 * 10**-3} in rad/mm \n m_eff = {eff_m_1} in kg \n V_eff = {V_eff_1}")
-print(f"Probe 2 \n A = {A_delta_phi_2 * 10 **-9} in rad/mm3 \n B = {B_delta_phi_2 * 10**-3} in rad/mm \n m_eff = {eff_m_2} in kg \n V_eff = {V_eff_2}")
+### Mit y-Achsenabschnitt ###
+#print(f"Probe 1 \n A = {A_delta_phi_1 * 10 **-9} in rad/mm3 \n B = {B_delta_phi_1 * 10**-3} in rad/mm \n m_eff = {eff_m_1} in kg \n V_eff = {V_eff_1}")
+#print(f"Probe 2 \n A = {A_delta_phi_2 * 10 **-9} in rad/mm3 \n B = {B_delta_phi_2 * 10**-3} in rad/mm \n m_eff = {eff_m_2} in kg \n V_eff = {V_eff_2}")
+### Mit y-Achsenabschnitt ###
+print(f"Probe 1 \n A = {A_delta_phi_1 * 10 **-9} in rad/mm3 \n m_eff = {eff_m_1} in kg \n V_eff = {V_eff_1}")
+print(f"Probe 2 \n A = {A_delta_phi_2 * 10 **-9} in rad/mm3 \n m_eff = {eff_m_2} in kg \n V_eff = {V_eff_2}")
+print(f"Mittelwerte:\n m_eff = {mean_eff} \n V_eff = {mean_V}")
 
 # Plotte alle Messwerte
 #fig,(ax1,ax2,ax3) = plt.subplots(3,1,figsize = (5,5))
@@ -101,13 +122,22 @@ print(f"Probe 2 \n A = {A_delta_phi_2 * 10 **-9} in rad/mm3 \n B = {B_delta_phi_
 #plt.tight_layout()
 #plt.savefig("Plots/Messwerte_normiert.pdf", bbox_inches = "tight")
 #plt.clf()
+
 fig,ax = plt.subplots(figsize = (5,3))
 plt.scatter((lam*10**6)**2, Phi_probe_rein * 10 **-3, color = "mediumvioletred", label = r"$\phi_\text{norm. rein}$")
 plt.ylabel(r"$\phi_\text{norm. rein} \, / \, \si{\radian\per\milli\meter}$")
+plt.xlabel(r"$\lambda^2 \, / \, \si{\micro\meter}$")
 plt.legend()
 plt.tight_layout()
 plt.savefig("Plots/Messwerte_rein.pdf")
 
+fig,ax = plt.subplots(figsize = (5,3))
+plt.scatter(1/(lam*10**6)**2, Phi_probe_rein * 10 **-3, color = "mediumvioletred", label = r"$\phi_\text{norm. rein}$")
+plt.ylabel(r"$\phi_\text{norm. rein} \, / \, \si{\radian\per\milli\meter}$")
+plt.xlabel(r"$\frac{1}{\lambda^2} \, / \, \si{\micro\meter}$")
+plt.legend()
+plt.tight_layout()
+plt.savefig("Plots/Messwerte_rein_reziprok.pdf")
 
 fig,ax = plt.subplots(figsize = (10,6))
 ax.scatter((lam*10**6)**2, delta_phi_1 * 10**-3, color = "hotpink",label = r"$\Delta \phi_\text{norm.1}$")
